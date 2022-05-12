37e8eede7754f6e84ba5b9435df0289e7d4bc20b
Ifeanyi Beniah Onyebueke <ifeanyionyebueke.ben@gmail.com>
2022-05-12 13:24:12 +0200

    no attempt yet

---
+++ starter-code/script.js
@@ -0,0 +1,17 @@
+<div id="main">
+  <button class="openbtn" onclick="openNav()">☰ Open Sidebar</button>
+  <h2>Collapsed Sidebar</h2>
+  <p>Click on the hamburger menu/bar icon to open the sidebar, and push this content to the right.</p>
+</div>
+
+<script>
+function openNav() {
+  document.getElementById("mySidebar").style.width = "250px";
+  document.getElementById("main").style.marginLeft = "250px";
+}
+
+function closeNav() {
+  document.getElementById("mySidebar").style.width = "0";
+  document.getElementById("main").style.marginLeft= "0";
+}
+</script>
